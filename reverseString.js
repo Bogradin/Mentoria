1cc460407c9541fd76c6ac9ec6b53703cb5b1828
Lucas Inacio <lucasinacio2003@hotmail.com>
2022-11-18 17:39:53 -0300

    reverseString should be working...

--- reverseString.js
+++ reverseString.js
@@ -1,2 +1,10 @@
 var reverseString = function(s) {
-// teste
+    let temporaryString;
+    let sLength = s.length;
+    for (let i = 0; i < sLength/2; i++)
+    {
+    temporaryString = s[i];
+    s[i] = s[sLength-i-1];
+    s[sLength-i-1] = temporaryString;
+    }
+};
