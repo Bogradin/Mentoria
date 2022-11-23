09df3c7f1f989ec942fe6aa6152e509f0296e1d5
Lucas Inacio <lucasinacio2003@hotmail.com>
2022-11-23 13:07:31 -0300

    rework

--- stringIntersec.js
+++ stringIntersec.js
@@ -1,18 +1,36 @@
-var stringIntersec = function(s) {
-    first = s[0];
-    second = s[1];
-    let intersec = [];
+var s = [[1, 3, 4, 7, 13], [1, 2, 4, 13, 15]];
+var first = s[0];
+var second = s[1];
+
+var order = function(s) {
+    
+    let orderedString = [];
 
     while (first.length && second.length) {
         if (first[0] < second[0]) {
-            intersec.push(first.shift())
+            orderedString.push(first.shift())
         } else {
-            intersec.push(second.shift())
+            orderedString.push(second.shift())
         }
     }
 
-    return [...intersec, ...first, ...second]
+    return [...orderedString, ...first, ...second]
 }
 
-s = [[1, 3, 4, 7, 13], [1, 2, 4, 13, 15]]
-stringIntersec(s)
+var intersec = function(s) {
+
+    let orderedString = order(s);
+    let intersec = [];
+
+    for (let i = 0; i < orderedString.length; i++){
+        for (let j = 0; j < orderedString.length; j++){
+            if (orderedString[i] = orderedString[j] && i < j) {
+                intersec.push(orderedString[i])
+            }
+            if (i > j) {
+                break
+            }
+        }
+    }
+    return intersec
+}
